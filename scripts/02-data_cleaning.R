c2ded9f01cdbd684c86b14619f98224a75fc96c5
leemarik <mariko.lee@mail.utoronto.ca>
2024-09-23 23:54:00 -0400

    Cleaned shelter system data for analysis

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,45 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the Toronto Shelter System dataset for analysis
+# Author:  Mariko Lee
+# Date: 23 September 2024
+# Contact: mariko.lee@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
+library(readr)
+library(dplyr)
+library(janitor)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/raw_data/toronto-shelter-system-flow.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+### Clean column names ###
+cleaned_data <- raw_data %>%
+  janitor::clean_names()
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+### Handle missing values ###
+cleaned_data <- cleaned_data %>%
+  drop_na()
+
+### Select relevant columns ###
+cleaned_data <- cleaned_data %>%
+  select(date, shelter_type, age_group, gender_male, gender_female, population)
+
+### Convert data types ###
+cleaned_data <- cleaned_data %>%
+  mutate(
+    date = as.Date(date, format = "%Y-%m-%d"),
+    gender_male = as.integer(gender_male),
+    gender_female = as.integer(gender_female),
+    population = as.integer(population)
+  )
+
+### Create new columns ###
+cleaned_data <- cleaned_data %>%
+  mutate(total_population = gender_male + gender_female)
+
+### Save cleaned data ###
+write_csv(cleaned_data, "data/analysis_data/cleaned_shelter_data.csv")
