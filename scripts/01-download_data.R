b4353243ae3fb47cc1fb395169603c533817eaef
leemarik <mariko.lee@mail.utoronto.ca>
2024-09-23 23:29:20 -0400

    Download Toronto Shelter System dataset

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,39 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the Toronto Shelter System dataset on Open Data Toronto
+# Author: Mariko Lee
+# Date: 23 September 2024
+# Contact: mariko.lee@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+if (!require(readr)) install.packages("readr")
+library(readr)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# get package info for the Toronto Shelter System Flow dataset
+package <- show_package("ac77f532-f18b-427c-905c-4ae87ce69c93")
+package
+
+# get all resources for this package
+resources <- list_package_resources("ac77f532-f18b-427c-905c-4ae87ce69c93")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+write_csv(data, "data/raw_data/toronto-shelter-system-flow.csv") 
 
          
